5328071dee6cb3903aff2b4964cf083d9087ed17
noisenuisance <sabauspne@gmail.com>
2022-10-23 18:09:15 +0300

    fix: add alt attr to LikedByPopup avatar img

--- src/components/LikedByPopup.js
+++ src/components/LikedByPopup.js
@@ -1,29 +1,33 @@
-import { useContext } from "react";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { useContext } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function LikedByPopup({ isOpen, onOverlay, likes }) {
   const currentUser = useContext(CurrentUserContext);
 
   return (
     <section
-      className={`popup popup_style_liked-by ${isOpen && "popup_status_show"}`}
+      className={`popup popup_style_liked-by ${isOpen && 'popup_status_show'}`}
       onClick={onOverlay}
-      onContextMenu={(e) => e.preventDefault()}
+      onContextMenu={e => e.preventDefault()}
     >
-      <div className="popup__container popup__liked-by-container">
+      <div className='popup__container popup__liked-by-container'>
         {likes &&
           likes.length > 0 &&
-          Array.from(likes).map((like) => (
-            <div className="popup__likes-user" key={like._id}>
-              <img className="popup__likes-avatar" src={like.avatar}></img>
-              <p className="popup__likes-name">
-                {like._id === currentUser._id ? "You" : like.name}
+          Array.from(likes).map(like => (
+            <div className='popup__likes-user' key={like._id}>
+              <img
+                className='popup__likes-avatar'
+                src={like.avatar}
+                alt={like.name}
+              ></img>
+              <p className='popup__likes-name'>
+                {like._id === currentUser._id ? 'You' : like.name}
               </p>
             </div>
           ))}
         {likes &&
           likes.length === 0 &&
-          "Be the first one to like this post ♡(>ᴗ•)"}
+          'Be the first one to like this post ♡(>ᴗ•)'}
       </div>
     </section>
   );
