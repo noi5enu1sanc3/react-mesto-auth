45047bb03ba467faf7ef98f76a79b609217fd8ca
noisenuisance <sabauspne@gmail.com>
2022-11-06 18:21:20 +0300

    fix: deployment settings

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,14 +1,10 @@
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect } from 'react-router-dom';
 
 function ProtectedRoute({ component: Component, ...props }) {
   return (
     <Route>
       {() =>
-        props.isLoggedIn ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="./sign-in" />
-        )
+        props.isLoggedIn ? <Component {...props} /> : <Redirect to='/sign-in' />
       }
     </Route>
   );
